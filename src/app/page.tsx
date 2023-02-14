36f1b92b5eb55f2e7f4750a6b39882fe869fa6a3
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-14 23:45:00 +0000

    Components and other things added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,42 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import TotalMoney from "@/components/TotalMoney";
+import Hero from "@/components/Hero";
+import Products from "@/components/Products";
+import Basket from "@/components/Basket";
+import SweetAlert from "@/components/SweetAlert";
+import { Products as ProductsData } from "@/data/products";
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  const [products, setProducts] = useState<Product[]>(ProductsData);
+  const [money, setMoney] = useState(128000000000);
+  const [basket, setBasket] = useState<Basket[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
-export default function Home() {
+  useEffect(() => {
+    setTotal(
+      basket.reduce((total, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return total + (product ? product.price * item.amount : 0);
+      }, 0)
+    );
+  }, [basket, products]);
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="space-y-8 w-9/12">
+      <Hero />
+      <TotalMoney total={total} money={money} />
+      <Products
+        total={total}
+        money={money}
+        products={products}
+        basket={basket}
+        setBasket={setBasket}
+      />
+      <Basket total={total} basket={basket} products={products} />
+      <SweetAlert basket={basket} money={money} products={products} setBasket={setBasket} total={total} />
     </main>
-  )
-}
+  );
+};
+
+export default Home;
