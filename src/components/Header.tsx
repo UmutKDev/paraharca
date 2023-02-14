421ed3cbe7c9984bbeb9e44282123b054192ad16
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-14 23:46:22 +0000

    Header Component Added

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='bg-white drop-shadow-2xl h-16 flex justify-center items-center w-full'>
+            <h1 className='text-3xl font-semibold'><span className='text-transparent bg-clip-text bg-gradient-to-r from-lime-400 to-green-500'>#</span>Paraharca</h1>
+        </header>
+    )
+}
+
+export default Header
