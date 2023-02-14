c226761722ef060d568d9a832190d55a854326aa
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-14 23:45:15 +0000

    font, header and footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
-import './globals.css'
+import "@/styles/globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="tr">
       <head />
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
