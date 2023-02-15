c818c62b45fef2e5a39756b9d2d12f1d893e7a93
Umut Kızıloğlu <umutkiziloglu00@gmail.com>
2023-02-15 10:43:49 +0300

    Update TotalMoney.tsx
    Signed-off-by: Umut Kızıloğlu <umutkiziloglu00@gmail.com>

--- src/components/TotalMoney.tsx
+++ src/components/TotalMoney.tsx
@@ -3,7 +3,7 @@ import { NumericFormat } from "react-number-format";
 
 const TotalMoney = ({ total, money }: any) => {
   return (
-    <div className="md:h-20 h-16 w-full flex flex-row bg-white drop-shadow-2xl rounded-xl sticky top-0 z-20">
+    <div className="md:h-20 h-16 w-full flex flex-row bg-white drop-shadow-2xl rounded-xl sticky top-4 z-20">
       <div className="flex items-center justify-center rounded-r-2xl rounded-md bg-gradient-to-r from-lime-400 to-green-500 md:w-1/12 w-3/12">
         <p className="md:text-4xl text-3xl text-white font-semibold">$</p>
       </div>
